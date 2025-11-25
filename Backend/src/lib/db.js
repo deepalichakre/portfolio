29dfebf6462a0f26e6fb733c993284afca6a36a1
Deepali Chakre <deepali.chakre@gmail.com>
2025-11-25 22:05:14 +0530

    Switch Prisma to @prisma/client and remove generated Windows client

--- Backend/src/lib/db.js
+++ Backend/src/lib/db.js
@@ -1,5 +1,5 @@
-import { PrismaClient } from "../generated/prisma/index.js";
-
+/*//import { PrismaClient } from "../generated/prisma/index.js";
+import { PrismaClient } from "@prisma/client";
 
 const g = globalThis;
 const prisma = g.__prisma || new PrismaClient({ log: ["warn", "error"] });
@@ -7,7 +7,7 @@ if (process.env.NODE_ENV !== "production") g.__prisma = prisma;
 
 export const db = prisma;
 
-/*
+
 // Backend/src/lib/db.js
 import { PrismaClient } from '@prisma/client';
 
@@ -15,3 +15,16 @@ const g = globalThis;
 export const db = g.__db || new PrismaClient({ log: ['warn', 'error'] });
 if (!g.__db) g.__db = db;
 */
+import { PrismaClient } from "@prisma/client";
+
+const g = globalThis;
+
+export const db =
+  g.__db ||
+  new PrismaClient({
+    log: ["warn", "error"],
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  g.__db = db;
+}
