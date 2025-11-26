0ff8698088dafb45a4a40e7d4be13daf47352f5a
Deepali Chakre <deepali.chakre@gmail.com>
2025-11-26 10:58:32 +0530

    Switch Prisma to @prisma/client, remove generated folder, use linux-compatible setup

--- Backend/src/lib/db.js
+++ Backend/src/lib/db.js
@@ -1,4 +1,4 @@
-import { PrismaClient } from "../generated/prisma/index.js";
+/*import { PrismaClient } from "../generated/prisma/index.js";
 //import { PrismaClient } from "@prisma/client";
 
 const g = globalThis;
@@ -6,14 +6,7 @@ const prisma = g.__prisma || new PrismaClient({ log: ["warn", "error"] });
 if (process.env.NODE_ENV !== "production") g.__prisma = prisma;
 
 export const db = prisma;
-
-/*
-// Backend/src/lib/db.js
-import { PrismaClient } from '@prisma/client';
-
-const g = globalThis;
-export const db = g.__db || new PrismaClient({ log: ['warn', 'error'] });
-if (!g.__db) g.__db = db;
+*/
 
 // Backend/src/lib/db.js
 import pkg from "@prisma/client";
@@ -31,4 +24,4 @@ export const db =
 if (process.env.NODE_ENV !== "production") {
   g.__db = db;
 }
-  */
+
