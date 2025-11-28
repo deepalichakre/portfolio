98800c4762e13080ab85971dfa97a3390b0c1151
Deepali Chakre <deepali.chakre@gmail.com>
2025-11-28 10:31:51 +0530

    Remove admin UI from deployed main branch

--- Frontend/src/app/admin/new/page.js
+++
@@ -1,8 +0,0 @@
-'use client';
-import { useState } from 'react';
-
-import AdminProjectForm from '../../../components/admin/AdminProjectForm';
-
-export default function AdminNewProjectPage() {
-  return <AdminProjectForm mode="new" initialProject={null} />;
-}
